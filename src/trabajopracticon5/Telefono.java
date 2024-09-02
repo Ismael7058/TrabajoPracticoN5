73d4b493afce5847fb1e083843bb0dc19ed087d6
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2024-09-02 18:20:27 -0300

    Modificacion de Clase Telefono

--- src/trabajopracticon5/Telefono.java
+++ src/trabajopracticon5/Telefono.java
@@ -9,5 +9,17 @@ package trabajopracticon5;
  * @author Usuario
  */
 public class Telefono {
-    
+    private long numero;
+
+    public Telefono(long numero) {
+        this.numero = numero;
+    }
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
 }
